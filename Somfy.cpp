5313e5d249cef9c4e2a1a7f16a460eb9c55be4e3
Robert Strouse <rstrouse@truepointsolutions.com>
2023-05-30 08:01:52 -0700

    Integrate pull request #54

--- Somfy.cpp
+++ Somfy.cpp
@@ -177,7 +177,7 @@ void somfy_frame_t::decodeFrame(byte* frame) {
             break;
         case somfy_commands::Status:
             this->rollingCode = 0;
-            this->status = decoded[3];
+            this->status = static_cast<somfy_status_t>(decoded[3]);
             break;
         case somfy_commands::UnknownC:
         case somfy_commands::UnknownD:
